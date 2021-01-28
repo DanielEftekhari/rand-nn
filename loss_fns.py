1cd3d59e35881050ccb2118b974dbf4fbd97be7f
Daniel Eftekhari <daniel.eftekhari.ai@gmail.com>
2021-01-28 14:27:51 -0500

    kl divergence loss function

--- loss_fns.py
+++ loss_fns.py
@@ -13,3 +13,13 @@ def cross_entropy_loss(logits, y):
     z = c + torch.log(torch.sum(torch.exp(logits - c[:, None]), dim=-1))
     losses = -torch.sum(y * (logits - z[:, None]), dim=-1)
     return losses
+
+
+def kl_p_to_u(logits, y):
+    max_ent = utils.max_ent(y.shape[1])
+    return max_ent - utils.entropy(logits)
+
+
+# def kl_u_to_p(logits, y):
+#     max_ent = utils.max_ent(y.shape[1])
+#     return -max_ent - 1. / y.shape[1] * torch.sum(utils.get_class_probs(logits), dim=-1)
