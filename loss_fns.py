fefda6d2def32b0d08c6b1c7a2c48f67b4141ad5
Daniel Eftekhari <daniel.eftekhari.ai@gmail.com>
2021-05-26 19:18:23 -0400

    1) correction to function <kl_u_to_p> 2) use kl divergence function <kl_y_to_p> as loss function (see commit description for details) 3) encapsulate numerically stable softmax in <get_class_probs> 4) pass probabilities, instead of logits, to function <entropy>, for ease of use 5) generalize <entropy> to allow zero probability terms without causing numerical instability 6) drop unused argument <y> in functions <kl_p_to_u> and <kl_u_to_p>
    details for 2): function <kl_y_to_p> generalizes function <cross_entropy_loss> to continuous label distributions, i.e. <kl_y_to_p> is equivalent to <cross_entropy_loss> for one-hot labels, and is a more sensible loss function for continuous label distributions

--- loss_fns.py
+++ loss_fns.py
@@ -15,11 +15,15 @@ def cross_entropy_loss(logits, y):
     return losses
 
 
-def kl_p_to_u(logits, y):
-    max_ent = utils.max_ent(y.shape[1])
-    return max_ent - utils.entropy(logits)
+def kl_p_to_u(logits):
+    max_ent = utils.max_ent(logits.shape[1])
+    return max_ent - utils.entropy(utils.get_class_probs(logits))
 
 
-def kl_u_to_p(logits, y):
-    max_ent = utils.max_ent(y.shape[1])
-    return -max_ent - 1. / y.shape[1] * torch.sum(utils.get_class_probs(logits), dim=-1)
+def kl_u_to_p(logits):
+    max_ent = utils.max_ent(logits.shape[1])
+    return -max_ent - 1. / logits.shape[1] * torch.sum(torch.log(utils.get_class_probs(logits)), dim=-1)
+
+
+def kl_y_to_p(logits, y):
+    return torch.sum(torch.where(y > 0, y * torch.log(y), torch.Tensor([0.]).to(logits.device)), dim=-1) - torch.sum(torch.where(y > 0, y * torch.log(utils.get_class_probs(logits)), torch.Tensor([0.]).to(logits.device)), dim=-1)
