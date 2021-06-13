ef438e33dc60d6476f49a93bd192084809f700f9
Daniel Eftekhari <daniel.eftekhari.ai@gmail.com>
2021-06-13 00:42:55 -0400

    add comments for improved clarity

--- config/utils.py
+++ config/utils.py
@@ -15,14 +15,17 @@ def update_params(params, args, membership):
     updates <params> with the key-value pairs in <args>.
     """
     for key in args:
+        # check that the key is a relevant parameter
         if key not in membership:
             continue
         
+        # trace the key back to the root of the object
         tree = [key]
         while key not in params:
             key = membership[key]
             tree.append(key)
         
+        # descend the tree, then update the parameter's value
         d = params
         for i in range(len(tree)-1, 0, -1):
             d = d[tree[i]]
